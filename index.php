d06ae53916dc0dca15fdd422201bc1829b5e8dbe
Sixky001 <6511130021@mut.ac.th>
2024-07-05 17:02:28 +0700

    commit3

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
      echo "<h1>My repository</h1>"
      echo "<h1>My name is six</h1>"
+     echo "<h1>edit index file</h1>";
+     echo "<h1>For send</h1>";
 ?>
--- menu.php
+++ menu.php
@@ -3,4 +3,6 @@
     echo "<h1>Product</h1>";
     echo "<h1>About</h1>";
     echo "<h1>Six</h1>";
+    echo "<h1>edit file for work</h1>";
+    echo "<h1>send to aj yai</h1>";
 ?>
