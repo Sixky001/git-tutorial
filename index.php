2264b20dc5c347b0a1021c588dcaa7c2ee15ba95
Sixky001 <6511130021@mut.ac.th>
2024-07-05 17:11:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,8 @@
      echo "<h1>My name is six</h1>"
      echo "<h1>edit index file</h1>";
      echo "<h1>For send</h1>";
+     echo "<h1>My repository</h1>"
+     echo "<h1>My name is six</h1>"
+     echo "<h1>edit index file</h1>";
+     echo "<h1>For send</h1>";
 ?>
